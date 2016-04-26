f139802a4c95bbe9db4b25564c5209627bd81888
Shawn <shawn@Shawns-MacBook-Air.local>
2016-04-26 16:21:19 -0600

    fix for setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+#brew tap homebrew/nginx
+#brew install openresty
 command -v luajit >/dev/null 2>&1 || { echo "I require luajit but it's not installed.  Aborting." >&2; exit 1; }
 command -v luarocks-5.1 >/dev/null 2>&1 || { echo "I require luarocks-5.1 but it's not installed.  Aborting." >&2; exit 1; }
 luarocks-5.1 install ffi
 luarocks-5.1 install --server=http://luarocks.org/dev lua2go
 luarocks-5.1 install busted
-busted ngx/lua/tests/**/*.lua
+busted ./ngx/lua/tests/**.lua
