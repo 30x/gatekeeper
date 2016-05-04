fea41fd70d6c839a85dfce227b498f775a7d082e
Shawn Feldman <shawn@Shawns-MacBook.local>
2016-05-04 08:23:12 -0600

    rest header

--- e2e/server/index.js
+++ e2e/server/index.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const restify = require('./rest')
+const port = process.env.REST_PORT || 8000
+restify(port,function(err,server){
+  console.log('successfully bound to rest port %s',port)
+})
