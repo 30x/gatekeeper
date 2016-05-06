45dde46c0910120ff503421e19e555d23a52ab97
shawn.feldman@gmail.com <shawn@Shawns-MacBook.local>
2016-05-06 06:06:12 -0600

    add uuid

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ command -v luajit >/dev/null 2>&1 || { echo "I require luajit but it's not insta
 command -v luarocks-5.1 >/dev/null 2>&1 || { echo "I require luarocks-5.1 but it's not installed.  Aborting." >&2; exit 1; }
 luarocks-5.1 install luasec OPENSSL_DIR=/usr/local/opt/openssl/
 luarocks-5.1 install lua-resty-http
+luarocks-5.1 install lua-resty-jit-uuid
 #luarocks-5.1 install --server=http://luarocks.org/dev lua2go
 luarocks-5.1 install busted
-busted ./ngx/lua/tests/**.lua
+./lua/tests/bustedjit ./ngx/lua/tests/**.lua
