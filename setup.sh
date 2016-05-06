fc0483ca370d520dfa2ddb6b0115ef5f100f25d9
shawn.feldman@gmail.com <shawn@Shawns-MacBook.fios-router.home>
2016-05-06 17:07:27 -0400

    brew options

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-#brew tap homebrew/nginx
-#brew install openresty
+#brew install homebrew/nginx/openresty
+#brew install lua5.1
+#brew install luajit
 command -v openresty >/dev/null 2>&1 || { echo "I require openresty but it's not installed.  Aborting." >&2; exit 1; }
 command -v luajit >/dev/null 2>&1 || { echo "I require luajit but it's not installed.  Aborting." >&2; exit 1; }
 command -v luarocks-5.1 >/dev/null 2>&1 || { echo "I require luarocks-5.1 but it's not installed.  Aborting." >&2; exit 1; }
