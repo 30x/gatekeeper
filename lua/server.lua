1afed8d4526c4813625b6cf42cf9c5fdf07b1800
shawn.feldman@gmail.com <shawn@Shawns-MacBook.local>
2016-05-09 14:37:51 -0600

    fixed

--- lua/server.lua
+++ lua/server.lua
@@ -13,8 +13,16 @@ function server.onrequest()
   local raw_headers = ngx.req.raw_header(true)
 
   local result = events.on_request(uri, method, raw_headers)
+  local lower_headers = {}
   for k,v in pairs(result.headers) do
     ngx.req.set_header(k,v)
+    lower_headers[string.lower(k)] = v
+  end
+  for k,v in pairs(ngx.req.get_headers()) do
+    if not lower_headers[k] then
+      print('deleting ' .. k)
+      ngx.req.set_header(k,nil)
+    end
   end
   ngx.req.set_uri(result.uri)
   -- ngx.req.set_method(somenumber)
