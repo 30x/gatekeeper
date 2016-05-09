04b6ee7498e15b33913117f1e05377f10dda0e55
shawn.feldman@gmail.com <shawn@Shawns-MacBook.local>
2016-05-09 14:58:00 -0600

    tests

--- lua/tests/events.lua
+++ lua/tests/events.lua
@@ -5,23 +5,23 @@ describe("a test", function()
   -- tests
   it("checks c struct", function()
     local events = require("../events")
-    local headers = "key1: val1\nkey2: val2\nkey3: val3"
+    local headers = "X-MyHeader-key1: val1\nX-MyHeader-key2: val2\nX-MyHeader-key3: val3"
 
     local res = events.on_request('http://someuri','PUT',headers);
-    assert.is_equal(res.headers.key1[1], 'val1' .. 'modified')
-    assert.is_equal(res.headers.key2[1], 'val2' .. 'modified')
+    assert.is_equal(res.headers['X-MyHeader-key1'][1], 'modifiedval1')
+    assert.is_equal(res.headers['X-MyHeader-key2'][1], 'modifiedval2')
 
   end)
 
   -- tests
   it("checks c struct with weird maps", function()
     local events = require("../events")
-    local headers = "key1: val1,val2\nkey2: val3,val4\nkey3: val3"
+    local headers = "X-MyHeader-key1: val1,val2\nX-MyHeader-key2: val3,val4\nX-MyHeader-key3: val3"
 
     local res = events.on_request('http://someuri','PUT',headers);
 
-    assert.is_equal(res.headers.key1[1], 'val1' .. 'modified')
-    assert.is_equal(res.headers.key2[1], 'val3' .. 'modified')
+    assert.is_equal(res.headers['X-MyHeader-key1'][1], 'modifiedval1')
+    assert.is_equal(res.headers['X-MyHeader-key2'][1], 'modifiedval3')
 
   end)
 
