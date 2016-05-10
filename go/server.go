fc7ee720763fb044a9d752dd8fa14cbc7db71c82
shawn.feldman@gmail.com <shawn@Shawns-MacBook.local>
2016-05-10 10:47:23 -0600

    fmt

--- go/server.go
+++ go/server.go
@@ -1,17 +1,17 @@
 package main
+
 /*
-*/
+ */
 import "C"
 
 import (
-    "log"
-    "os"
-    "strings"
-    "bytes"
-	 "net/http"
-   "net/url"
-    )
-
+	"bytes"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
 
 //LogInfo the info log level
 var LogInfo = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
@@ -23,82 +23,81 @@ var LogWarn = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.LUTC|log.Lsho
 var LogError = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
 
 //export process
-func process(uri string, method string, rawHeaders string) (*C.char, *C.char, *C.char){
-  //parse uri
-  parsedUri, err := url.Parse(uri)
-  if err != nil {
-    LogError.Print(err)
-  }
-  headerMap := make(http.Header)
-  parseHeaders(headerMap, rawHeaders)
+func process(uri string, method string, rawHeaders string) (*C.char, *C.char, *C.char) {
+	//parse uri
+	parsedUri, err := url.Parse(uri)
+	if err != nil {
+		LogError.Print(err)
+	}
+	headerMap := make(http.Header)
+	parseHeaders(headerMap, rawHeaders)
 
-  //start something with headers
-  modifyHeaders(headerMap)
-  //end
+	//start something with headers
+	modifyHeaders(headerMap)
+	//end
 
-  //serialize map back to string
-  serializedHeaders := serializeHeaders(headerMap)
-  // log.Print("headers are ")
-  // log.Print( serializedHeaders)
+	//serialize map back to string
+	serializedHeaders := serializeHeaders(headerMap)
+	// log.Print("headers are ")
+	// log.Print( serializedHeaders)
 
 	return C.CString(parsedUri.String()), C.CString(method), C.CString(serializedHeaders)
 }
-func modifyHeaders(headerMap http.Header){
-  for k := range headerMap {
-    if strings.Contains(k,"X-MyHeader") {
-      for i:=0; i <  len(headerMap[k]); i++ {
-        newVal := "modified" + headerMap[k][i]
-        headerMap[k][i] = newVal
+func modifyHeaders(headerMap http.Header) {
+	for k := range headerMap {
+		if strings.Contains(k, "X-MyHeader") {
+			for i := 0; i < len(headerMap[k]); i++ {
+				newVal := "modified" + headerMap[k][i]
+				headerMap[k][i] = newVal
 
-      }
-    }
-  }
+			}
+		}
+	}
 }
-func serializeHeaders(headerMap http.Header) string{
-  var buffer bytes.Buffer
-  for key := range headerMap {
-    values := headerMap[key]
-    var valuesBuffer bytes.Buffer
-    for i := 0; i < len(values); i++ {
-      if values[i] != "" {
-        if(i > 0){
-          valuesBuffer.WriteString(",")
-        }
-        valuesBuffer.WriteString(values[i])
-      }
-    }
-    val := valuesBuffer.String()
-    buffer.WriteString(key)
-    buffer.WriteString(": ")
-    buffer.WriteString(val)
-    buffer.WriteString("\n")
-  }
-  serializedHeaders := buffer.String()
+func serializeHeaders(headerMap http.Header) string {
+	var buffer bytes.Buffer
+	for key := range headerMap {
+		values := headerMap[key]
+		var valuesBuffer bytes.Buffer
+		for i := 0; i < len(values); i++ {
+			if values[i] != "" {
+				if i > 0 {
+					valuesBuffer.WriteString(",")
+				}
+				valuesBuffer.WriteString(values[i])
+			}
+		}
+		val := valuesBuffer.String()
+		buffer.WriteString(key)
+		buffer.WriteString(": ")
+		buffer.WriteString(val)
+		buffer.WriteString("\n")
+	}
+	serializedHeaders := buffer.String()
 
-  return serializedHeaders
+	return serializedHeaders
 }
 
-func parseHeaders(headerMap http.Header, rawHeaders string){
-  headerValues := strings.Split(rawHeaders,"\n")
-  for _, header := range headerValues  {
-    keyValue := strings.Split(header,": ")
-    if(len(keyValue) == 2){
-      key := keyValue[0]
-      valueString := keyValue[1]
-      if valueString != "" {
-        values := strings.Split(valueString,",")
-        if _, ok := headerMap[key] ; !ok{
-          headerMap[key] = make([]string,len(values))
-        }
-        for i:=0; i < len(values); i++ {
-          headerMap[key][i] = values[i]
-        }
-      }
-    }
+func parseHeaders(headerMap http.Header, rawHeaders string) {
+	headerValues := strings.Split(rawHeaders, "\n")
+	for _, header := range headerValues {
+		keyValue := strings.Split(header, ": ")
+		if len(keyValue) == 2 {
+			key := keyValue[0]
+			valueString := keyValue[1]
+			if valueString != "" {
+				values := strings.Split(valueString, ",")
+				if _, ok := headerMap[key]; !ok {
+					headerMap[key] = make([]string, len(values))
+				}
+				for i := 0; i < len(values); i++ {
+					headerMap[key][i] = values[i]
+				}
+			}
+		}
 
-  }
+	}
 }
 func main() {
 
 }
-
