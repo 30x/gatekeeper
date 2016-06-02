7e6c52a36e9cd578e330c43bfb534b124ae20d8f
Shawn Feldman <shawn@Shawns-MacBook.local>
2016-06-02 14:23:05 -0600

    setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+#brew install homebrew/nginx/openresty
+#brew install lua51
+#brew install luajit
+command -v openresty >/dev/null 2>&1 || { echo "I require openresty but it's not installed.  Aborting." >&2; exit 1; }
+
 git clone git@github.com:30x/lua-gozerian.git lua
 pushd lua
 ./setup.sh
