9a49755e772549e20535f02ae94b4b37166f0f1b
Shawn Feldman <shawn@Shawns-MacBook.local>
2016-06-09 12:44:15 -0600

    files

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 command -v openresty >/dev/null 2>&1 || { echo "I require openresty but it's not installed.  Aborting." >&2; exit 1; }
 #rm -rf lua
 git clone git@github.com:30x/lua-gozerian.git lua
-pushd lua
-chmod +x setup.sh
-./setup.sh
-popd
+command -v luajit >/dev/null 2>&1 || { echo "I require luajit but it's not installed.  Aborting." >&2; exit 1; }
+command -v luarocks-5.1 >/dev/null 2>&1 || { echo "I require luarocks-5.1 but it's not installed.  Aborting." >&2; exit 1; }
+luarocks-5.1 install luasec OPENSSL_DIR=/usr/local/opt/openssl/
+luarocks-5.1 install --server=http://luarocks.org/dev lua-gozerian
